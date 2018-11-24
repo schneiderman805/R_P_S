6d8f94e272ef8bf8cc76b3338b1ae2b8bf805bf6
William Schneiderman <william.m.schneiderman@gmail.com>
2018-11-24 08:57:57 +0100

    finishes point system functions with display message function

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userPoints = 0;
-const computerPoints = 0;
+var userPoints = 0;
+var computerPoints = 0;
 const userPoints_span_tag = document.getElementById("user_points");
 const computerPoints_span_tag = document.getElementById("computer_points");
 const scoreCount_div_tag = document.querySelector(".score_count");
-const result_div_tag = document.querySelector(".result");
+const result_p_tag = document.querySelector(".result > p");
 const rock_div_tag = document.getElementById("rock");
 const paper_div_tag = document.getElementById("paper");
 const scissor_div_tag = document.getElementById("scissor");
@@ -15,25 +15,48 @@ function getComputerOption() {
 }
 
 
+function win(userOption, computerOption) {
+    userPoints++;
+    userPoints_span_tag.innerHTML = userPoints;
+    computerPoints_span_tag.innerHTML = computerPoints;
+    result_p_tag.innerHTML = userOption + " beats " + computerOption + ". you win! ";
+    
+
+}
+
+function lose(userOption, computerOption) {
+    computerPoints++;
+    userPoints_span_tag.innerHTML = userPoints;
+    computerPoints_span_tag.innerHTML = computerPoints;
+    result_p_tag.innerHTML = userOption + " loses against  " + computerOption + ". you lose! ";
+    
+
+}
+
+function draw(userOption, computerOption) {
+    result_p_tag.innerHTML = userOption + " = " + computerOption + ". It's a draw! ";
+
+}
+
 function game(userOption) {
     const computerOption = getComputerOption();
     switch (userOption + computerOption) {
         case "rockscissor":
         case "paperrock":
         case "scissorpaper":
-        win();
+        win(userOption, computerOption);
         break;
 
         case "rockpaper":
         case "paperscissor":
         case "scissorrock":
-        lose();
+        lose(userOption, computerOption);
         break;
 
         case "rockrock":
         case "paperpaper":
         case "scissorscissor":
-        draw();
+        draw(userOption, computerOption);
         break;
 
 
